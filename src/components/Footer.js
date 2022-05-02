8c85ee45270574a00ababcd1947b8c27e93b1db5
Bharath S <36877669+bharathsampangi@users.noreply.github.com>
2022-05-02 11:49:37 +0530

    completed css grid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,46 @@
 import React from "react";
+import LogoLight from "../img/logo_light.png";
 
 export default function Footer() {
     return (
-        <footer>
-            Footer
+        <footer className="footer">
+            <div className="footer__container row">
+                <div className="footer__main">
+                    <div>
+                        <img className="logo" src={LogoLight} alt="Logo Light" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a metus auctor, scelerisque felis
+                            et, lobortis purus. Praesent suscipit mauris lorem, sed elementum nibh fringilla quis.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>email newsletter</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <input placeholder="Enter email..."/>
+                        <button className="btn btn-primary btn-large">Subscribe</button>
+                    </div>
+                    <div>
+                        <h3>site links</h3>
+                        <ul>
+                            <li>Help & Support</li>
+                            <li>Privacy & Policy</li>
+                            <li>About Us</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Join our club</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a metus auctor, scelerisque felis
+                            et, lobortis purus.
+                        </p>
+                        <button className="btn btn-secondary">Join Now</button>
+                    </div>
+                </div>
+                <div className="footer__copyright">
+                    <p>Copyrights &copy; All Rights Reserved</p>
+                </div>
+            </div>
         </footer>
-    )
+    );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Articles from "../components/Articles";
+import Footer from "../components/Footer"
 
 export default function Home() {
     return (
         <>
             <Header />
             <Articles />
+            <Footer />
         </>
     )
 }
